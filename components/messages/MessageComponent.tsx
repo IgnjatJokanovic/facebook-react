718dab4845b2f84b77ebe9d504c00fb3ca637f9f
Ignjat Jokanovic <jokanovic.ignjat@gmail.com>
2022-11-19 19:14:23 +0100

    Messages

--- components/messages/MessageComponent.tsx
+++ components/messages/MessageComponent.tsx
@@ -1,5 +1,12 @@
+import { useForm } from "react-hook-form";
 
 export default function MessageComponent() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    defaultValues: {
+      message: ''
+    }
+  });
+  const onSubmit = data => console.log(data);
   return (
     <div className="message-component">
         <div className="header">
@@ -7,9 +14,51 @@ export default function MessageComponent() {
                 <img src="https://dummyimage.com/300.png/09f/fff" alt="" />
                 <p>Ignjat Jokanovic</p>
             </div>
-            <span className="close">X</span>  
+        <div className="navigation">
+          <i className="fa fa-window-minimize" aria-hidden="true"></i>
+          <i className="fa fa-window-close" aria-hidden="true"></i>
+        </div>  
         </div>
         <div className="body">
+          <div className="messages">
+            <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble">
+                  Test
+              </div>
+              <div className="bubble right">
+                  Test
+              </div>
+             
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-inputs">
+                <textarea placeholder="Write message" rows="2"></textarea>
+                <div className="controlls">
+                  <i className="fas fa-smile"></i>
+                  <button>Send</button>
+                </div>
+                
+              </div>
+             
+              {errors.message && <span className="error">Message field is required</span> }
+            </form>
             
         </div>
     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,15 @@ import Navbar from '../components/Navbar'
 import { Suspense } from 'react'
 import Pageloader from '../components/loaders/Pageloader'
 import MessagesContainer from '../components/messages/MessagesContainer'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Facebook</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" />
+      </Head>
       <Suspense fallback={ <Pageloader /> }>
         <Navbar />
           <Component {...pageProps} />
