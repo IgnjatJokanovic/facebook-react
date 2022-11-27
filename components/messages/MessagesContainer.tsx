8cf574a8dfe21b0cf6c31af8d4e8acafbcb26a44
Ignjat Jokanovic <jokanovic.ignjat@gmail.com>
2022-11-27 21:45:08 +0100

    finished basic messages layout

--- components/messages/MessagesContainer.tsx
+++ components/messages/MessagesContainer.tsx
@@ -8,6 +8,7 @@ export default function MessagesContainer() {
         <MessageComponent />
         <MessageComponent />
         <MessageComponent />
+        <MessageComponent />
         
     </div>
   )
