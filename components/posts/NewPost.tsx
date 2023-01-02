a9eb35178807d74275b7a08fb66dca49b8beddf8
Ignjat Jokanovic <jokanovic.ignjat@gmail.com>
2023-01-02 21:06:01 +0100

    Finished photo section of new post

--- components/posts/NewPost.tsx
+++ components/posts/NewPost.tsx
@@ -4,6 +4,15 @@ import AddImage from './newPost/AddImage';
 import TagFriends from './newPost/TagFriends';
 
 export default function NewPost({ article, setArticle }) {
+
+    const refFile = React.useRef(null);
+    const openFile = () => {
+        refFile.current.click();
+    }
+
+    const updateImage = (image) => {
+        setArticle({...article, image: image});
+    }
     
     const handleSubmit = () => {
         console.log(article)
@@ -13,13 +22,26 @@ export default function NewPost({ article, setArticle }) {
         <form onSubmit={e => {
             e.preventDefault();
             handleSubmit();
-        }}>
+          }}>
+            {article.image != null ? (
+                <div className="image-container">
+                    <div className="controlls">
+                        <span onClick={openFile}>
+                            Edit
+                        </span> 
+                          <span onClick={() => updateImage(null)}>
+                            Delete
+                        </span>  
+                    </div>
+                    <img src={ article.image } alt="" />      
+                </div>      
+            ) : "" }
             <div className="insert-options">
                 <div className="title">
                     Add to post
                 </div>
                 <div className="controlls">
-                    <AddImage />
+                      <AddImage post={article} setPost={setArticle} refFile={refFile} openFile={openFile} updateImage={ updateImage } />
                       <TagFriends taged={article.taged} setArticle={setArticle} />
                     {/* <AddEmotion />   */}
                 </div>
--- components/posts/newPost/AddImage.tsx
+++ components/posts/newPost/AddImage.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
-export default function AddImage() {
+export default function AddImage({ post, setPost, openFile, refFile, updateImage }) {
+  
+
+
+  const getBase46 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        }
+        fileReader.onerror = (error) => {
+            reject(error);
+        }
+    });
+  };
+
+  const handleFileRead = async (e) => {
+      const file = e.target.files[0];
+      if (file.type.match('image.*')) {
+        const base64 = await getBase46(file);
+        updateImage(base64);
+      } else {
+        //handle error
+        console.log('error')
+      }
+      
+  };
+
+  
   return (
       <div className='controll-item image'>
         <div className="icon-holder">
             <div className="dropdown-title">Add image</div>
-            <i className="fas fa-image image-icon" title='Add image'></i>
+            <i className="fas fa-image image-icon" title='Add image' onClick={openFile}></i>
+            <input ref={refFile} type="file" className="d-none" onChange={e => handleFileRead(e)}/>
         </div>
         
     </div>
