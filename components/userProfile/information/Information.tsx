644f1fb3d633f613cd39efe05f1f2a1e3c6a317a
Ignjat Jokanovic <jokanovic.ignjat@gmail.com>
2023-05-23 21:58:32 +0200

    friends

--- components/userProfile/information/Information.tsx
+++ components/userProfile/information/Information.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function Information({ userId }) {
+export default function Information({ firstName, lastName, birthDay, showEdit, userId }) {
   return (
-    <div>Information</div>
+    <div className='information-container'>
+      <div className="content">
+        <div className="item">
+          First Name: {firstName}
+        </div>
+        <div className="item">
+          Last Name: {lastName}
+        </div>
+        <div className="item">
+          Birthday: {birthDay}
+        </div>
+
+        {showEdit && (
+          <div className="item">
+            <Link href={`/user/update`} className="btn">
+              Edit
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
